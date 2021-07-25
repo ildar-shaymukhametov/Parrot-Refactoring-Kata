90a65a1f49da43a8a3ace74de16867d678da7415
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-07-25 20:29:09 +0500

    remove unused

--- CSharp/Parrot/Parrot.cs
+++ CSharp/Parrot/Parrot.cs
@@ -4,44 +4,18 @@ namespace Parrot
 {
     public class Parrot
     {
-        private readonly bool _isNailed;
-        private readonly int _numberOfCoconuts;
-        private readonly ParrotTypeEnum _type;
-        private readonly double _voltage;
-
-        public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
-        {
-            _type = type;
-            _numberOfCoconuts = numberOfCoconuts;
-            _voltage = voltage;
-            _isNailed = isNailed;
-        }
-
         public virtual double GetSpeed()
         {
             return 12.0;
         }
-
-        private double GetBaseSpeed(double voltage)
-        {
-            return Math.Min(24.0, voltage * GetSpeed());
-        }
-
-        private double GetLoadFactor()
-        {
-            return 9.0;
-        }
     }
 
-    public class EuropeanParrot : Parrot
-    {
-        public EuropeanParrot() : base(default, default, default, default) { }
-    }
+    public class EuropeanParrot : Parrot { }
 
     public class AfricanParrot : Parrot
     {
         private readonly int _numberOfCoconuts;
-        public AfricanParrot(int numberOfCoconuts) : base(default, default, default, default)
+        public AfricanParrot(int numberOfCoconuts)
         {
             _numberOfCoconuts = numberOfCoconuts;
         }
@@ -61,7 +35,7 @@ namespace Parrot
     {
         private readonly double _voltage;
         private readonly bool _isNailed;
-        public NorwegianParrot(double voltage, bool isNailed) : base(default, default, default, default)
+        public NorwegianParrot(double voltage, bool isNailed)
         {
             _voltage = voltage;
             _isNailed = isNailed;
